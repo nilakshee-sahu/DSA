cf2e194755676e8a013b68329f49a4340bec7820
nilakshee-sahu <157611746+nilakshee-sahu@users.noreply.github.com>
2026-01-13 19:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (54.98%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    pair<bool, int> isBalancedFast(TreeNode* root){
+        // base case
+        if(root == NULL){
+            pair <int, int> p = make_pair(true,0);
+            return p;
+        }
+        
+        pair <int, int> left = isBalancedFast(root->left);
+        pair <int, int> right = isBalancedFast(root->right);
+        
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+        bool diff = abs(left.second - right.second) <= 1;
+        
+        pair<int, int> ans;
+        ans.second = max(left.second, right.second) + 1;
+        
+        if(leftAns && rightAns && diff)
+            ans.first = true;
+        else 
+            ans.first = false;
+
+        return ans;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        return isBalancedFast(root).first;
+    }
+
+};
