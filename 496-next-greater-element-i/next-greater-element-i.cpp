b3fd9e6711033711f1610200844bb5d4bd05e84a
Nilakshee Sahu <157611746+nilakshee-sahu@users.noreply.github.com>
2026-07-22 18:23:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (15.81%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,22 +3,23 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
-        vector<int> nge(n1);
-        // stack<int> st;
 
-        for(int i=0; i<n1; i++){
-            auto idx = find(nums2.begin(), nums2.end(), nums1[i]);
-            int index = distance(nums2.begin(), idx);
-            for(int j=index+1; j<n2; j++){
-                if(nums2[j]>nums2[index]){
-                    nge[i] = nums2[j];
-                    break;
-                }
-                nge[i] = -1;
-            }
+        unordered_map<int, int> mp;   
+        stack<int> st;
+
+        for(int i=n2-1; i>=0; i--){
+            while(!st.empty() && st.top() <= nums2[i])
+                st.pop();
+
+            if(st.empty()) mp[nums2[i]] = -1;
+            else mp[nums2[i]] = st.top();
+
+            st.push(nums2[i]);
         }
+
+        vector<int> nge(n1);
         for(int i=0; i<n1; i++){
-            if(nge[i] == 0) nge[i] = -1;
+            nge[i] = mp[nums1[i]];
         }
         return nge;
     }
